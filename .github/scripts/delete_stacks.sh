b44b306b55e763050aa3b3da308c86b59bec316f
Kris Szlapa <kris.szlapa1@nhs.net>
2024-03-08 11:36:56 +0000

    Fix delete_stacks.sh script

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^prescription-status-update-pr-(sandbox-)?(\\d+)$") ) | .StackName ')
+ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^psu-(sandbox-)?pr-(\\d+)$") ) | .StackName ')
 
 mapfile -t ACTIVE_STACKS_ARRAY <<< "$ACTIVE_STACKS"
 
 for i in "${ACTIVE_STACKS_ARRAY[@]}"
 do 
   echo "Checking if stack $i has open pull request"
-  PULL_REQUEST=${i//pr-/}
-  PULL_REQUEST=${PULL_REQUEST//sandbox-/}
+  PULL_REQUEST=${i//psu-pr-/}
+  PULL_REQUEST=${PULL_REQUEST//psu-sandbox-pr-/}
   echo "Checking pull request id ${PULL_REQUEST}"
   URL="https://api.github.com/repos/NHSDigital/eps-prescription-status-update-api/pulls/${PULL_REQUEST}"
   RESPONSE=$(curl "${URL}" 2>/dev/null)
