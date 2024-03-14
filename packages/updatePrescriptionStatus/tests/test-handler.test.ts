906d20d5b0c9f4ed94680f1d2895619d1008e2b7
Kris Szlapa <kris.szlapa1@nhs.net>
2024-03-14 16:52:41 +0000

    Refactor the unit test for app handler

--- packages/updatePrescriptionStatus/tests/test-handler.test.ts
+++ packages/updatePrescriptionStatus/tests/test-handler.test.ts
@@ -3,6 +3,7 @@ import {handler} from "../src/updatePrescriptionStatus"
 import {DynamoDBClient} from "@aws-sdk/client-dynamodb"
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
 import {mockClient} from "aws-sdk-client-mock"
+
 import exampleInCollectionLocker from "../../specification/examples/request-in-collection-locker.json"
 import exampleMultipleItems from "../../specification/examples/request-multiple-items.json"
 import exampleMissingFields from "../../specification/examples/request-missing-fields.json"
@@ -26,34 +27,33 @@ const generateMockEvent = (body: any): APIGatewayProxyEvent => ({
 })
 
 describe("Unit test for app handler", () => {
-  it("should return 201 with response bundle for a single item", async () => {
-    const event: APIGatewayProxyEvent = generateMockEvent(exampleInCollectionLocker)
-    const response: APIGatewayProxyResult = await handler(event, {} as any)
+  it.each([
+    {
+      example: exampleInCollectionLocker,
+      httpResponseCode: 201,
+      scenarioDescription: "201 with response bundle for a single item"
+    },
+    {
+      example: exampleMultipleItems,
+      httpResponseCode: 201,
+      scenarioDescription: "201 with response bundle for multiple items"
+    },
+    {
+      example: exampleNoItems,
+      httpResponseCode: 200,
+      scenarioDescription: "200 status code if there are no entries to process"
+    }
+  ])(
+    "should return $scenarioDescription",
+    async ({example, httpResponseCode}) => {
+      const event: APIGatewayProxyEvent = generateMockEvent(example)
+      const response: APIGatewayProxyResult = await handler(event, {} as any)
 
-    expect(response.statusCode).toBe(201)
-    const responseBody = JSON.parse(response.body!)
-    expect(responseBody).toHaveProperty("resourceType", "Bundle")
-  })
-
-  it("should return 201 with response bundle for multiple items", async () => {
-    const event: APIGatewayProxyEvent = generateMockEvent(exampleMultipleItems)
-    const response: APIGatewayProxyResult = await handler(event, {} as any)
-
-    expect(response.statusCode).toBe(201)
-    const responseBody = JSON.parse(response.body!)
-    expect(responseBody).toHaveProperty("resourceType", "Bundle")
-  })
-
-  it("should return 200 status code if there are no entries to process", async () => {
-    const event: APIGatewayProxyEvent = generateMockEvent(exampleNoItems)
-    const response: APIGatewayProxyResult = await handler(event, {} as any)
-    expect(response.statusCode).toBe(200)
-    expect(JSON.parse(response.body)).toEqual({
-      resourceType: "Bundle",
-      type: "transaction-response",
-      entry:[]
+      expect(response.statusCode).toBe(httpResponseCode)
+      const responseBody = JSON.parse(response.body!)
+      expect(responseBody).toHaveProperty("resourceType", "Bundle")
+      expect(responseBody).toHaveProperty("type", "transaction-response")
     })
-  })
 
   it("should return a 400 status code and error message indicating missing required fields", async () => {
     const event: APIGatewayProxyEvent = generateMockEvent(exampleMissingFields)
