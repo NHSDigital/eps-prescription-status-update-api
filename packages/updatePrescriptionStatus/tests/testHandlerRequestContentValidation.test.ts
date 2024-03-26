eb4f793446b51bb06d028f2b0b52179065862ad9
Phil Gee <originalphil@gmail.com>
2024-03-26 17:15:00 +0000

    AEA-3830 Testing multiple validation issues at handler level.

--- packages/updatePrescriptionStatus/tests/testHandlerRequestContentValidation.test.ts
+++ packages/updatePrescriptionStatus/tests/testHandlerRequestContentValidation.test.ts
@@ -9,11 +9,16 @@ import {
 } from "@jest/globals"
 
 import {handler} from "../src/updatePrescriptionStatus"
-import {DEFAULT_DATE, TASK_ID_0, generateMockEvent} from "./utils/testUtils"
+import {
+  DEFAULT_DATE,
+  TASK_ID_0,
+  TASK_ID_1,
+  generateMockEvent
+} from "./utils/testUtils"
 import {ONE_DAY_IN_MS} from "../src/requestContentValidation"
 
-import exampleDispatched from "../../specification/examples/request-dispatched.json"
-import {badRequest, bundleWrap} from "../src/utils/responses"
+import requestMultipleItems from "../../specification/examples/request-multiple-items.json"
+import {accepted, badRequest, bundleWrap} from "../src/utils/responses"
 
 describe("Unit test for validation via updatePrescriptionStatus handler", () => {
   beforeEach(() => {
@@ -21,14 +26,39 @@ describe("Unit test for validation via updatePrescriptionStatus handler", () => 
     jest.useFakeTimers().setSystemTime(DEFAULT_DATE)
   })
 
-  it("when content validation issues are present, expect a 400 status code and 400 response code(s)", async () => {
-    const body: any = {...exampleDispatched}
+  it("when one content validation issue is present in multiple items, expect 400 status code, message indicating validation issues, and message indicating valid item", async () => {
+    const body: any = {...requestMultipleItems}
 
     const now = new Date()
     body.entry[0].resource.lastModified = new Date(now.valueOf() + ONE_DAY_IN_MS + 100000).toISOString()
+
     const event: APIGatewayProxyEvent = generateMockEvent(body)
 
-    const expected = bundleWrap([badRequest("Invalid last modified value provided.", TASK_ID_0)])
+    const expected = bundleWrap([
+      badRequest("Invalid last modified value provided.", TASK_ID_0),
+      accepted(TASK_ID_1)
+    ])
+
+    const response: APIGatewayProxyResult = await handler(event, {})
+
+    expect(response.statusCode).toBe(400)
+    expect(JSON.parse(response.body!)).toEqual(expected)
+  })
+
+  it("when multiple items all have content validation issues, expect 400 status code and messages indicating validation issues", async () => {
+    const body: any = {...requestMultipleItems}
+
+    const now = new Date()
+    body.entry[0].resource.lastModified = new Date(now.valueOf() + ONE_DAY_IN_MS + 100000).toISOString()
+
+    delete body.entry[1].resource.basedOn
+
+    const event: APIGatewayProxyEvent = generateMockEvent(body)
+
+    const expected = bundleWrap([
+      badRequest("Invalid last modified value provided.", TASK_ID_0),
+      badRequest("Prescription ID is invalid.", TASK_ID_1)
+    ])
 
     const response: APIGatewayProxyResult = await handler(event, {})
 
