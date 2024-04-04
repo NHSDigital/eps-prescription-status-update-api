0d535dc4bce946bb398ecd1648de5ab92c82af91
Kris Szlapa <kris.szlapa1@nhs.net>
2024-04-04 08:30:58 +0000

    Update the proxygen path

--- .github/scripts/deploy_api.sh
+++ .github/scripts/deploy_api.sh
@@ -19,6 +19,7 @@ echo "$client_private_key" > /tmp/client_private_key.pem
 
 environment=internal-dev
 instance=prescription-status-update
+path_to_spec=./.aws-sam/build/proxygen
 path_to_spec=packages/specification/dist/eps-prescription-status-update-api.resolved.json
 
 # Create ~/.proxygen directory if it doesn't exist
@@ -40,4 +41,4 @@ endpoint_url: https://proxygen.prod.api.platform.nhs.uk
 spec_output_format: json
 EOF
 
-"${PROXYGEN_PATH}" instance deploy --no-confirm "$environment" "$instance" "$path_to_spec"
+"$path_to_spec" instance deploy --no-confirm "$environment" "$instance" "$path_to_spec"
