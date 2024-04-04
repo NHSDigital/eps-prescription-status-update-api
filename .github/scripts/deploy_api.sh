6727f9cdf3dd7bb41a50c6f027a6d8a9b0cf1770
Kris Szlapa <kris.szlapa1@nhs.net>
2024-04-04 10:24:07 +0000

    Get the directory where proxygen is installed

--- .github/scripts/deploy_api.sh
+++ .github/scripts/deploy_api.sh
@@ -19,8 +19,8 @@ echo "$client_private_key" > /tmp/client_private_key.pem
 
 environment=internal-dev
 instance=prescription-status-update
-path_to_proxygen=./.aws-sam/build/proxygen
-path_to_spec=packages/specification/dist/eps-prescription-status-update-api.resolved.json
+path_to_proxygen="$PROXYGEN_PATH"
+path_to_spec="$spec_path"
 
 # Create ~/.proxygen directory if it doesn't exist
 mkdir -p ~/.proxygen
@@ -41,8 +41,9 @@ endpoint_url: https://proxygen.prod.api.platform.nhs.uk
 spec_output_format: json
 EOF
 
-cd ../../.aws-sam/build || exit
-make publish
+
+# cd ../../.aws-sam/build || exit
+# make publish
 
 # Navigate back to the previous directory
 cd - >/dev/null || exit
