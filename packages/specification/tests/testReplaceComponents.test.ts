afbf707f65ae335558e67e366ce6bdcd5db8f9c5
Kris Szlapa <kris.szlapa1@nhs.net>
2024-04-05 11:23:46 +0000

    Add specification package to sonar-project properties

--- packages/specification/tests/testReplaceComponents.test.ts
+++ packages/specification/tests/testReplaceComponents.test.ts
@@ -1,5 +1,5 @@
 import fs from "fs"
-import {replaceAppLevel0Object} from "../scripts/replaceComponents"
+import {replaceAppLevel0Object} from "../scr/replaceComponents"
 
 describe("replaceAppLevel0Object", () => {
   const jsonFilePath: string = "dist/eps-prescription-status-update-api.resolved.json"
