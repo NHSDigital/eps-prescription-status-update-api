cb37ec2a23246dd63dfee3715f8908d15f3da4a5
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-04-17 10:01:49 +0000

    delete old apigee instances

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -20,3 +20,48 @@ do
     echo "not going to delete stack $i as state is ${STATE}"
   fi
 done
+
+# Retrieve the proxygen private key and client private key and cert from AWS Secrets Manager
+proxygen_private_key_arn=$(aws cloudformation list-exports --query "Exports[?Name=='account-resources:ProxgenPrivateKey'].Value" --output text)
+proxygen_private_key=$(aws secretsmanager get-secret-value --secret-id "${proxygen_private_key_arn}" --query SecretString --output text)
+
+# Save the proxygen private key, client private key, and client cert to temporary files
+echo "$proxygen_private_key" > ~/.proxygen/tmp/proxygen_private_key.pem
+
+# Create the .proxygen/tmp directory if it doesn't exist
+mkdir -p ~/.proxygen/tmp
+# Create credentials.yaml file
+cat <<EOF > ~/.proxygen/credentials.yaml
+client_id: prescription-status-update-api-client
+key_id: eps-cli-key-1
+private_key_path: tmp/proxygen_private_key.pem
+base_url: https://identity.prod.api.platform.nhs.uk/realms/api-producers
+client_secret: https://nhsdigital.github.io/identity-service-jwks/jwks/paas/prescription-status-update-api.json
+EOF
+
+# Create settings.yaml file
+cat <<EOF > ~/.proxygen/settings.yaml
+api: prescription-status-update-api
+endpoint_url: https://proxygen.prod.api.platform.nhs.uk
+spec_output_format: json
+EOF
+
+ACTIVE_APIGEE=$(poetry run proxygen instance list --env internal-dev | awk 'NR > 2 {print $3}')
+mapfile -t ACTIVE_APIGEE_ARRAY <<< "$ACTIVE_APIGEE"
+
+for i in "${ACTIVE_APIGEE_ARRAY[@]}"
+do
+  echo "Checking if apigee deployment $i has open pull request"
+  PULL_REQUEST=${i//prescription-status-update-pr-/}
+  PULL_REQUEST=${PULL_REQUEST//-sandbox/}
+  echo "Checking pull request id ${PULL_REQUEST}"
+    URL="https://api.github.com/repos/NHSDigital/eps-prescription-status-update-api/pulls/${PULL_REQUEST}"
+  RESPONSE=$(curl "${URL}" 2>/dev/null)
+  STATE=$(echo "${RESPONSE}" | jq -r .state)
+  if [ "$STATE" == "closed" ]; then
+    echo "** going to delete apigee deployment $i as state is ${STATE} **"
+    poetry run proxygen instance delete --no-confirm "${i}"
+  else
+    echo "not going to delete apigee deployment $i as state is ${STATE}"
+  fi
+done
