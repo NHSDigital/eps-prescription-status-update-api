72b689e98bcf9b4c992dede5fe15659fe95e838f
natasafrgk <natasa.fragkou@airelogic.com>
2024-05-21 12:45:38 +0000

    comments out for code coverage

--- packages/updatePrescriptionStatus/src/utils/responses.ts
+++ packages/updatePrescriptionStatus/src/utils/responses.ts
@@ -119,61 +119,61 @@ export function serverError(): BundleEntry {
   }
 }
 
-export function conflictDuplicate(taskId: string): BundleEntry {
-  return {
-    response: {
-      status: "409 Conflict",
-      location: `Task/${taskId}`,
-      lastModified: new Date().toISOString(),
-      outcome: {
-        resourceType: "OperationOutcome",
-        meta: {
-          lastUpdated: new Date().toISOString()
-        },
-        issue: [
-          {
-            code: "duplicate",
-            severity: "error",
-            details: {
-              coding: [
-                {
-                  system: "https://fhir.nhs.uk/CodeSystem/http-error-codes",
-                  code: "REC_CONFLICT",
-                  display: "409: The Receiver identified a conflict."
-                }
-              ]
-            },
-            diagnostics:
-              "Request contains a task id and prescription id identical to a record already in the data store."
-          }
-        ]
-      }
-    }
-  }
-}
+// export function conflictDuplicate(taskId: string): BundleEntry {
+//   return {
+//     response: {
+//       status: "409 Conflict",
+//       location: `Task/${taskId}`,
+//       lastModified: new Date().toISOString(),
+//       outcome: {
+//         resourceType: "OperationOutcome",
+//         meta: {
+//           lastUpdated: new Date().toISOString()
+//         },
+//         issue: [
+//           {
+//             code: "duplicate",
+//             severity: "error",
+//             details: {
+//               coding: [
+//                 {
+//                   system: "https://fhir.nhs.uk/CodeSystem/http-error-codes",
+//                   code: "REC_CONFLICT",
+//                   display: "409: The Receiver identified a conflict."
+//                 }
+//               ]
+//             },
+//             diagnostics:
+//               "Request contains a task id and prescription id identical to a record already in the data store."
+//           }
+//         ]
+//       }
+//     }
+//   }
+// }
 
-export function acceptedDuplicate(): BundleEntry {
-  return {
-    response: {
-      status: "200 OK",
-      lastModified: new Date().toISOString(), //double check
-      outcome: {
-        resourceType: "OperationOutcome",
-        meta: {
-          lastUpdated: new Date().toISOString()
-        },
-        issue: [
-          {
-            code: "informational",
-            severity: "information",
-            diagnostics:
-              "No issues detected during validation. Resource not created as request contains a duplicate task id."
-          }
-        ]
-      }
-    }
-  }
-}
+// export function acceptedDuplicate(): BundleEntry {
+//   return {
+//     response: {
+//       status: "200 OK",
+//       lastModified: new Date().toISOString(), //double check
+//       outcome: {
+//         resourceType: "OperationOutcome",
+//         meta: {
+//           lastUpdated: new Date().toISOString()
+//         },
+//         issue: [
+//           {
+//             code: "informational",
+//             severity: "information",
+//             diagnostics:
+//               "No issues detected during validation. Resource not created as request contains a duplicate task id."
+//           }
+//         ]
+//       }
+//     }
+//   }
+// }
 
 export function createSuccessResponseEntries(entries: Array<BundleEntry>) {
   return entries.map((e) => created(e.fullUrl!))
