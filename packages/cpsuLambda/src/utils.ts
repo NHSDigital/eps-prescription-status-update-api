530a58ff5787841272ac2193688e28fdd3f25faa
Jack Spagnoli <jack.spagnoli1@nhs.net>
2024-07-08 10:14:58 +0000

    updates use of utility function

--- packages/cpsuLambda/src/utils.ts
+++ packages/cpsuLambda/src/utils.ts
@@ -1,35 +1,4 @@
 import {APIGatewayProxyResult} from "aws-lambda"
-import {Err, Ok, Result} from "pratica"
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Array<T> {
-    /**
-     * Collects an array of results into a result of an array.
-     * If all results are Ok, returns an Ok with an array of values.
-     * If any result is an Err, returns an Err with an array of errors.
-     */
-    all_ok<O, E>(this: Array<Result<O, E>>): Result<Array<O>, Array<E>>
-  }
-}
-
-Array.prototype.all_ok = function <O, E>(this: Array<Result<O, E>>): Result<Array<O>, Array<E>> {
-  const successes: Array<O> = []
-  const failures: Array<E> = []
-
-  for (const result of this) {
-    result.cata({
-      Ok: (value) => successes.push(value),
-      Err: (error) => failures.push(error)
-    })
-  }
-
-  if (failures.length > 0) {
-    return Err(failures)
-  } else {
-    return Ok(successes)
-  }
-}
 
 declare global {
   interface String {
