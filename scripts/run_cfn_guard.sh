05b7e6da96fda4fc06e666f1ba48790ff2eb47d7
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-08-13 16:44:42 +0000

    check transformed files

--- scripts/run_cfn_guard.sh
+++ scripts/run_cfn_guard.sh
@@ -9,10 +9,19 @@ curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aws-cloud
 
 mkdir -p cfn_guard_output
 
-~/.guard/bin/cfn-guard validate \
-    --data SAMtemplates \
-    --rules /tmp/ruleset/output/ncsc.guard \
-    --show-summary fail \
-    > cfn_guard_output/SAMtemplates.txt
+while IFS= read -r -d '' file
+do
+    echo "checking $file"
+    mkdir -p "$(dirname cfn_guard_output/"$file")"
+
+    sam validate -t "$file" --debug 2>&1 | \
+    grep -Pazo '(?s)AWSTemplateFormatVersion.*\n\n' | \
+    tr -d '\0' | \
+    ~/.guard/bin/cfn-guard validate \
+        --rules /tmp/ruleset/output/ncsc.guard \
+        --show-summary fail \
+        > "cfn_guard_output/$file".txt
+
+done <   <(find ./SAMtemplates -name '*.y*ml' -print0)
 
 rm -rf /tmp/ruleset
