42bab39361ac7c99d14b4751662aaf9a5904e175
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-08-13 11:37:31 +0100

    Fix: [AEA-4341] - auto delete old records in dynamodb (#661)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - enable TTL on dynamo table so that records are auto deleted

--- packages/updatePrescriptionStatus/src/updatePrescriptionStatus.ts
+++ packages/updatePrescriptionStatus/src/updatePrescriptionStatus.ts
@@ -22,6 +22,8 @@ import {
 import {TransactionCanceledException} from "@aws-sdk/client-dynamodb"
 
 const LAMBDA_TIMEOUT_MS = 9500
+// this is length of time from now when records in dynamodb will automatically be expired
+export const TTL_DELTA = 60 * 60 * 24 * 365 * 2 // Keep records for 2 years
 const logger = new Logger({serviceName: "updatePrescriptionStatus"})
 
 export interface DataItem {
@@ -36,6 +38,7 @@ export interface DataItem {
   TaskID: string
   TerminalStatus: string
   ApplicationName: string
+  ExpiryTime: number
 }
 
 const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -207,7 +210,8 @@ export function buildDataItems(
       Status: task.businessStatus!.coding![0].code!,
       TaskID: task.id!,
       TerminalStatus: task.status,
-      ApplicationName: applicationName
+      ApplicationName: applicationName,
+      ExpiryTime: (Math.floor(+new Date() / 1000) + TTL_DELTA)
     }
 
     dataItems.push(dataItem)
