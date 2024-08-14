fb76c47188063ea37f7d907c8c86a2621676b0ba
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-08-14 08:24:10 +0000

    more checks

--- scripts/run_cfn_guard.sh
+++ scripts/run_cfn_guard.sh
@@ -2,6 +2,8 @@
 set -eou pipefail
 
 rm -rf /tmp/ruleset
+rm -rf cfn_guard_output
+
 wget -O /tmp/ruleset.zip https://github.com/aws-cloudformation/aws-guard-rules-registry/releases/download/1.0.2/ruleset-build-v1.0.2.zip  >/dev/null 2>&1
 unzip /tmp/ruleset.zip -d /tmp/ruleset/  >/dev/null 2>&1 
 
@@ -9,19 +11,26 @@ curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/aws-cloud
 
 mkdir -p cfn_guard_output
 
-while IFS= read -r -d '' file
-do
-    echo "checking $file"
-    mkdir -p "$(dirname cfn_guard_output/"$file")"
+declare -a rulesets=("ncsc" "ncsc-cafv3" "wa-Reliability-Pillar" "wa-Security-Pillar")
+for ruleset in "${rulesets[@]}"
+    do
 
-    sam validate -t "$file" --region eu-west-2 --debug 2>&1 | \
-    grep -Pazo '(?s)AWSTemplateFormatVersion.*\n\n' | \
-    tr -d '\0' | \
-    ~/.guard/bin/cfn-guard validate \
-        --rules /tmp/ruleset/output/ncsc.guard \
-        --show-summary fail \
-        > "cfn_guard_output/$file".txt
+    while IFS= read -r -d '' file
+    do
+        echo "checking SAM template $file with ruleset $ruleset"
+        mkdir -p "$(dirname cfn_guard_output/"$file")"
 
-done <   <(find ./SAMtemplates -name '*.y*ml' -print0)
+        # transform the SAM template to cloudformation and then run through cfn-guard
+        sam validate -t "$file" --region eu-west-2 --debug 2>&1 | \
+        grep -Pazo '(?s)AWSTemplateFormatVersion.*\n\n' | \
+        tr -d '\0' | \
+        ~/.guard/bin/cfn-guard validate \
+            --rules "/tmp/ruleset/output/$ruleset.guard" \
+            --show-summary fail \
+            > "cfn_guard_output/${file}_${ruleset}.txt"
+
+    done <   <(find ./SAMtemplates -name '*.y*ml' -print0)
+
+done
 
 rm -rf /tmp/ruleset
