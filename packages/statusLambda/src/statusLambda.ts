94a1c503c786aee835b6cbebc8c58b1a216625b6
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-03-12 19:03:16 +0000

    add a trace message

--- packages/statusLambda/src/statusLambda.ts
+++ packages/statusLambda/src/statusLambda.ts
@@ -30,6 +30,7 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
   })
   logger.info("This is a log after keys have been added")
 
+  logger.trace("This is a trace message")
   logger.debug("This is a debug message")
   logger.info("This is an info message")
   logger.warn("This is a warn message")
