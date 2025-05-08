01e36ad8c0b52c1c4a2eaf069533275925f74e3a
Jim Wild <james.wild6@nhs.net>
2025-05-08 09:06:23 +0000

    Check signature

--- packages/nhsNotifyUpdateCallback/src/lambdaHandler.ts
+++ packages/nhsNotifyUpdateCallback/src/lambdaHandler.ts
@@ -9,15 +9,60 @@ import httpHeaderNormalizer from "@middy/http-header-normalizer"
 
 import errorHandler from "@nhs/fhir-middy-error-handler"
 
+import {createHmac} from "crypto"
+
 export const logger = new Logger({serviceName: "nhsNotifyUpdateCallback"})
+
+const APP_NAME = process.env.APP_NAME ?? "NO-APP-NAME"
+const API_KEY = process.env.API_KEY ?? "NO-API-KEY"
+
+function response(statusCode: number, body: unknown = {}) {
+  return {
+    statusCode,
+    body: JSON.stringify(body)
+  }
+}
+
+/**
+ * Checks the incoming NHS Notify request signature.
+ * If it's okay, returns undefined.
+ * If it's not okay, it returns the error response object.
+ */
+function checkSignature(event: APIGatewayProxyEvent) {
+  const signature = event.headers["x-hmac-sha256-signature"]
+  if (!signature) return response(401)
+
+  const givenApiKey = event.headers["x-api-key"]
+  if (!givenApiKey) return response(401)
+
+  const secretValue = `${APP_NAME}.${API_KEY}`
+
+  // Compute the HMAC-SHA256 hash of the combination of the request body and the secret value
+  const payload = event.body ?? ""
+  const expectedSignature = createHmac("sha256", secretValue)
+    .update(payload, "utf8")
+    .digest("hex")
+
+  const givenSignature = event.headers["x-hmac-sha256-signature"]
+  if (givenSignature !== expectedSignature) return response(403)
+
+  return undefined
+}
 
 const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.appendKeys({
     "x-correlation-id": event.headers["x-correlation-id"],
-    "apigw-request-id": event.headers["apigw-request-id"]
+    "apigw-request-id": event.headers["apigw-request-id"],
+    "x-request-id": event.headers["x-request-id"] ?? "x-request-id-not-given"
   })
-
   logger.info("Lambda called with this event", {event})
+
+  // Require a request ID
+  if (!event.headers["x-request-id"]) return response(401)
+
+  // Check the request signature
+  const isErr = checkSignature(event)
+  if (isErr) return isErr
 
   return {
     statusCode: 201,
