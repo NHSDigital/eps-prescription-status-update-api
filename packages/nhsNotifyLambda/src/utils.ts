2f253db2c44fd0cc10766849284b6ff1ca6b2af0
Jim Wild <james.wild6@nhs.net>
2025-07-01 10:18:05 +0100

    Fix: [AEA-0000] - fix silent running (#1858)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    Silent running seems to be stuck on. This is to investigate and fix that

--- packages/nhsNotifyLambda/src/utils.ts
+++ packages/nhsNotifyLambda/src/utils.ts
@@ -44,20 +44,25 @@ const dynamo = new DynamoDBClient({region: process.env.AWS_REGION})
 const docClient = DynamoDBDocumentClient.from(dynamo, {marshallOptions})
 
 const ssm = new SSMProvider()
-const paramNames = {
-  [process.env.MAKE_REAL_NOTIFY_REQUESTS_PARAM!]: {maxAge: 60},
-  [process.env.NOTIFY_API_BASE_URL_PARAM!]: {maxAge: 60}
-}
-const configPromise = ssm.getParametersByName(paramNames)
 
 async function loadConfig(): Promise<{
   makeRealNotifyRequests: boolean,
   notifyApiBaseUrlRaw: string
 }> {
-  const all = await configPromise
+  const paramNames = {
+    [process.env.MAKE_REAL_NOTIFY_REQUESTS_PARAM!]: {maxAge: 5},
+    [process.env.NOTIFY_API_BASE_URL_PARAM!]: {maxAge: 5}
+  }
+  const all = await ssm.getParametersByName(paramNames)
+
+  // make sure that the MAKE_REAL_NOTIFY_REQUESTS_PARAM parameter value is a string, and lowercase
+  const realNotifyParam = (all[process.env.MAKE_REAL_NOTIFY_REQUESTS_PARAM!] as string)
+    .toString()
+    .toLowerCase()
+    .trim()
 
   return {
-    makeRealNotifyRequests: all[process.env.MAKE_REAL_NOTIFY_REQUESTS_PARAM!] === "true",
+    makeRealNotifyRequests: realNotifyParam === "true",
     notifyApiBaseUrlRaw: all[process.env.NOTIFY_API_BASE_URL_PARAM!] as string
   }
 }
