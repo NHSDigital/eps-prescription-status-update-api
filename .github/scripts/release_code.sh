03863aedeb7215473b039016b5556acc0b202c4a
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-08-01 08:54:51 +0000

    more tags

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -14,4 +14,38 @@ LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTOR
 export LATEST_TRUSTSTORE_VERSION
 
 cd ../../.aws-sam/build || exit
-make sam-deploy-package
+
+REPO=eps-prescription-status-update-api
+CFN_DRIFT_DETECTION_GROUP="psu"
+if [[ "$STACK_NAME" =~ -pr-[0-9]+$ ]]; then
+  CFN_DRIFT_DETECTION_GROUP="psu-pull-request"
+fi
+
+sam deploy \
+    --template-file "$TEMPLATE_FILE" \
+    --stack-name "$STACK_NAME" \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+    --region eu-west-2 \
+    --s3-bucket "$artifact_bucket" \
+    --s3-prefix "$ARTIFACT_BUCKET_PREFIX" \
+    --config-file samconfig_package_and_deploy.toml \
+    --no-fail-on-empty-changeset \
+    --role-arn "$cloud_formation_execution_role" \
+    --no-confirm-changeset \
+    --force-upload \
+    --tags "version=$VERSION_NUMBER stack=$STACK_NAME repo=$REPO cfnDriftDetectionGroup=$CFN_DRIFT_DETECTION_GROUP" \
+    --parameter-overrides \
+            TruststoreVersion="$LATEST_TRUSTSTORE_VERSION" \
+            EnableMutualTLS="$ENABLE_MUTUAL_TLS" \
+            EnableSplunk=true \
+            EnableDynamoDBAutoScaling="$DYNAMODB_AUTOSCALE" \
+            VersionNumber="$VERSION_NUMBER" \
+            CommitId="$COMMIT_ID" \
+            LogLevel="$LOG_LEVEL" \
+            LogRetentionInDays="$LOG_RETENTION_DAYS" \
+            Environment="$TARGET_ENVIRONMENT" \
+            DeployCheckPrescriptionStatusUpdate="$DEPLOY_CHECK_PRESCRIPTION_STATUS_UPDATE" \
+            EnableAlerts="$ENABLE_ALERTS" \
+            StateMachineLogLevel="$STATE_MACHINE_LOG_LEVEL" \
+            EnableNotificationsInternal="$ENABLE_NOTIFICATIONS_INTERNAL" \
+            EnableNotificationsExternal="$ENABLE_NOTIFICATIONS_EXTERNAL"
