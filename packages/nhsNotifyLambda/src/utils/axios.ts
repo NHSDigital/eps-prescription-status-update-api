72627d5f7f9a0ef676a15beb7d16d18936aff1da
Jim Wild <james.wild6@nhs.net>
2025-09-12 15:35:27 +0000

    Try looser conditions

--- packages/nhsNotifyLambda/src/utils/axios.ts
+++ packages/nhsNotifyLambda/src/utils/axios.ts
@@ -21,20 +21,21 @@ export function setupAxios(
     logger.warn(`Call to notify failed - retrying. Retry count ${retryCount}`, {error})
   }
 
+  const onFinalRetry = () => {
+    logger.error("Call to notify failed, and retry budget exceeded. Stopping")
+  }
+
   axiosRetry(axiosInstance, {
     retries: 5,
     // exponential backoff honors Retry-After automatically if present
     retryDelay: axiosRetry.exponentialDelay,
     // IMPORTANT: Retry POSTs too — on network errors, 5xx, 429, or timeouts
     retryCondition: (error) => {
-      const status = error.response?.status
-      return (
-        axiosRetry.isNetworkError(error) || // DNS/TCP reset/etc.
-                (typeof status === "number" && status >= 400) || // catch all error codes
-                error?.code === "ECONNABORTED" // request timeout
-      )
+      const status = error.response?.status as number
+      return status >= 400 // catch all error codes
     },
     onRetry: onAxiosRetry,
+    onMaxRetryTimesExceeded: onFinalRetry,
     shouldResetTimeout: true
   })
 
