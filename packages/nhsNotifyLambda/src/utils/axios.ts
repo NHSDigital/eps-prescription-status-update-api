9b427f58a340680509ba76135413d6248d9cb460
Jim Wild <james.wild6@nhs.net>
2025-09-12 15:19:29 +0000

    Move axios setup function to its own file

---
+++ packages/nhsNotifyLambda/src/utils/axios.ts
@@ -0,0 +1,42 @@
+import {Logger} from "@aws-lambda-powertools/logger"
+
+import axios from "axios"
+import axiosRetry from "axios-retry"
+
+export function setupAxios(
+  logger: Logger,
+  notifyBaseUrl: string,
+  requestTimeout: number = 30_000
+): ReturnType<typeof axios.create> {
+  const axiosInstance = axios.create({
+    baseURL: notifyBaseUrl,
+    timeout: requestTimeout,
+    headers: {
+      Accept: "*/*"
+    }
+  })
+
+  // Retry configuration for transient failures and throttling
+  const onAxiosRetry = (retryCount: number, error: unknown) => {
+    logger.warn(`Call to notify failed - retrying. Retry count ${retryCount}`, {error})
+  }
+
+  axiosRetry(axiosInstance, {
+    retries: 5,
+    // exponential backoff honors Retry-After automatically if present
+    retryDelay: axiosRetry.exponentialDelay,
+    // IMPORTANT: Retry POSTs too — on network errors, 5xx, 429, or timeouts
+    retryCondition: (error) => {
+      const status = error.response?.status
+      return (
+        axiosRetry.isNetworkError(error) || // DNS/TCP reset/etc.
+                (typeof status === "number" && status >= 400) || // catch all error codes
+                error?.code === "ECONNABORTED" // request timeout
+      )
+    },
+    onRetry: onAxiosRetry,
+    shouldResetTimeout: true
+  })
+
+  return axiosInstance
+}
