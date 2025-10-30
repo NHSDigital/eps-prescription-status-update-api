57bffa2d5266418b9cf8ad5f08a256ec73819ae4
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2025-10-30 10:58:39 +0000

    And the next
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/common/middyErrorHandler/src/errorHandler.ts
+++ packages/common/middyErrorHandler/src/errorHandler.ts
@@ -28,7 +28,7 @@ class MiddyErrorHandler {
         // if there are a `statusCode` and an `error` field
         // this is a valid http error object
         if (typeof (logger as Logger)[level] === "function") {
-          (logger as any)[level](
+          (logger as Logger)[level](
             {
               error: ((e) => ({
                 name: e.name,
