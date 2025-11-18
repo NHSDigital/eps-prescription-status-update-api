688ddc4b9c7af541edc82bb4afbd6264f94a51c0
Tim Stephenson <231503406+tstephen-nhs@users.noreply.github.com>
2025-11-18 10:29:26 +0000

    chore: reference central msg list but do not change

--- packages/common/utilities/src/index.ts
+++ packages/common/utilities/src/index.ts
@@ -1,8 +1,8 @@
 export {testPrescriptionsConfig, TestPrescriptionsConfig, getTestPrescriptions} from "./testConfig.js"
 export const LOG_MESSAGES = {
-  PSU0001: "PSU0001 Transitioning item status.",
+  PSU0001: "Transitioning item status.",
   PSU0002: "PSU0002 Notify request",
-  PSU0003: "PSU0003 Updated notification state"
+  PSU0003: "Updated notification state"
 } as const
 
 export {initiatedSSMProvider} from "./ssmUtil.js"
