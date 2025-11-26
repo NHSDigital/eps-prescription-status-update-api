cd4a0b9fa42cdf8bd1862752c051566852469c3b
Tim Stephenson <231503406+tstephen-nhs@users.noreply.github.com>
2025-11-26 10:32:55 +0000

    chore: script the aws secrets as well as token exchange

--- scripts/do-notify-token-exchange.sh
+++ scripts/do-notify-token-exchange.sh
@@ -5,15 +5,15 @@ export STACK="${STACK:-secrets}" # old = `secrets`, new = `psu`
 
 export NOTIFY_API_BASE_URL="https://int.api.service.nhs.uk"
 
-if [ "$STACK" = "psu" ]; then
-  NOTIFY_API_KEY="$(aws secretsmanager get-secret-value --secret-id "psu-PSU-Notify-API-Key" --query SecretString --output text)"
-else
-  NOTIFY_API_KEY="$(aws secretsmanager get-secret-value --secret-id "secrets-PSU-Notify-Application-Name" --query SecretString --output text)"
-fi
+NOTIFY_API_KEY="$(aws secretsmanager get-secret-value --secret-id "psu-PSU-Notify-API-Key" --query SecretString --output text)"
 export NOTIFY_API_KEY
 NOTIFY_PRIVATE_KEY="$(aws secretsmanager get-secret-value --secret-id "psu-PSU-Notify-PrivateKey" --query SecretString --output text)"
 export NOTIFY_PRIVATE_KEY
-NOTIFY_KID="$(aws secretsmanager get-secret-value --secret-id "psu-PSU-Notify-KID" --query SecretString --output text)"
+if [ "$STACK" = "psu" ]; then
+  NOTIFY_KID="$(aws secretsmanager get-secret-value --secret-id "psu-PSU-Notify-KID" --query SecretString --output text)"
+else
+  NOTIFY_API_KEY="$(aws secretsmanager get-secret-value --secret-id "secrets-PSU-Notify-Application-Name" --query SecretString --output text)"
+fi
 export NOTIFY_KID
 
 echo "Seeking token exchange with Notify for profile '$AWS_PROFILE' with '$STACK' secrets"
