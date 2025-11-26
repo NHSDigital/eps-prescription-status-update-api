b69e40f2e8f3d5efb3b8d65ebf0361db0545179a
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2025-11-26 15:26:58 +0000

    Transition to declared log message for reporting protection
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/updatePrescriptionStatus/src/updatePrescriptionStatus.ts
+++ packages/updatePrescriptionStatus/src/updatePrescriptionStatus.ts
@@ -33,6 +33,7 @@ import {
   testPrescription2Intercept
 } from "./utils/testPrescriptionIntercept"
 import {getTestPrescriptions, initiatedSSMProvider} from "@psu-common/utilities"
+import {LOG_MESSAGES} from "@psu-common/utilities"
 
 export const LAMBDA_TIMEOUT_MS = 9500
 // this is length of time from now when records in dynamodb will automatically be expired
@@ -365,7 +366,7 @@ export function buildDataItems(
   for (const requestEntry of requestEntries) {
     const task = requestEntry.resource as Task
     // The following log line is utilised for PSU "with Pharmacy" reporting, required at info level for Prod running
-    logger.info("Building data item for task.", {task: task, id: task.id})
+    logger.info(LOG_MESSAGES.PSU0004, {task: task, id: task.id})
 
     const repeatNo = task.input?.[0]?.valueInteger
 
