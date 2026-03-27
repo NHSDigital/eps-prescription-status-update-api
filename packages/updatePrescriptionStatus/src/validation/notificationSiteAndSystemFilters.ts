801e127ea18cbb463e2aae5b3b9520b68b6cb109
Jim Wild <james.wild6@nhs.net>
2026-03-27 12:03:14 +0000

    implement the ID checking in parallel, with env flag to toggle between id and name checkign

--- packages/updatePrescriptionStatus/src/validation/notificationSiteAndSystemFilters.ts
+++ packages/updatePrescriptionStatus/src/validation/notificationSiteAndSystemFilters.ts
@@ -2,6 +2,10 @@ import {PSUDataItemWithPrevious} from "@psu-common/commonTypes"
 import {initiatedSSMProvider} from "@psu-common/utilities"
 import {Logger} from "@aws-lambda-powertools/logger"
 
+const USE_PRODUCT_ID_FOR_NOTIFICATIONS_FILTERING =
+  (process.env.USE_PRODUCT_ID_FOR_NOTIFICATIONS_FILTERING || "false")
+    .toLowerCase() === "true"
+
 function str2set(value: string | undefined): Set<string> {
   const raw = value ?? ""
   return new Set(raw
@@ -13,22 +17,26 @@ function str2set(value: string | undefined): Set<string> {
 
 async function loadConfig(): Promise<{
   enabledSiteODSCodes: Set<string>,
+  enabledSystemAppNames: Set<string>,
   enabledSystemAppIds: Set<string>,
   blockedSiteODSCodes: Set<string>
 }> {
   const paramNames = {
     [process.env.ENABLED_SITE_ODS_CODES_PARAM!]: {maxAge: 5},
     [process.env.ENABLED_SYSTEMS_PARAM!]: {maxAge: 5},
+    [process.env.ENABLED_PRODUCT_IDS_PARAM!]: {maxAge: 5},
     [process.env.BLOCKED_SITE_ODS_CODES_PARAM!]: {maxAge: 5}
   }
   const all = await initiatedSSMProvider.getParametersByName(paramNames)
 
   const enabledSiteODSCodes = str2set(all[process.env.ENABLED_SITE_ODS_CODES_PARAM!] as string)
-  const enabledSystemAppIds = str2set(all[process.env.ENABLED_SYSTEMS_PARAM!] as string)
+  const enabledSystemAppNames = str2set(all[process.env.ENABLED_SYSTEMS_PARAM!] as string)
+  const enabledSystemAppIds = str2set(all[process.env.ENABLED_PRODUCT_IDS_PARAM!] as string)
   const blockedSiteODSCodes = str2set(all[process.env.BLOCKED_SITE_ODS_CODES_PARAM!] as string)
 
   return {
     enabledSiteODSCodes,
+    enabledSystemAppNames,
     enabledSystemAppIds,
     blockedSiteODSCodes
   }
@@ -48,17 +56,30 @@ export async function checkSiteOrSystemIsNotifyEnabled(
   logger?: Logger
 ): Promise<Array<PSUDataItemWithPrevious>> {
   // Get the configuration from either the cache or SSM
-  const {enabledSiteODSCodes, enabledSystemAppIds, blockedSiteODSCodes} = await loadConfig()
+  const {enabledSiteODSCodes, enabledSystemAppNames, enabledSystemAppIds, blockedSiteODSCodes} = await loadConfig()
   const unfilteredItemCount = data.length
 
   const filteredItems = data.filter((item) => {
     const appId = item.current.ApplicationID.trim().toLowerCase()
+    const appName = item.current.ApplicationName.trim().toLowerCase()
     const odsCode = item.current.PharmacyODSCode.trim().toLowerCase()
 
-    // Is this item either ODS enabled, or supplier enabled?
-    const isEnabledSystem = enabledSiteODSCodes.has(odsCode) || enabledSystemAppIds.has(appId)
-    if (!isEnabledSystem) {
-      return false
+    logger?.info(
+      "Product ID, application name, and ODS code",
+      {productId: appId, applicationName: appName, odsCode, enabledSystemAppIds}
+    )
+
+    // Is this item supplier enabled?
+    if (USE_PRODUCT_ID_FOR_NOTIFICATIONS_FILTERING) {
+      const isEnabledProduct = enabledSiteODSCodes.has(odsCode) || enabledSystemAppIds.has(appId)
+      if (!isEnabledProduct) {
+        return false
+      }
+    } else {
+      const isEnabledSystem = enabledSiteODSCodes.has(odsCode) || enabledSystemAppNames.has(appId)
+      if (!isEnabledSystem) {
+        return false
+      }
     }
 
     // Cannot have a blocked ODS code
