4245e4232439d3c6b65c4c94110fe0b1b4a6cc30
Anthony Brown <anthony.brown8@nhs.net>
2026-05-01 09:53:51 +0000

    check vars

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -6,28 +6,43 @@ export AWS_MAX_ATTEMPTS
 echo "$COMMIT_ID"
 
 CF_LONDON_EXPORTS=$(aws cloudformation list-exports --region eu-west-2 --output json)
-artifact_bucket=$(echo "$CF_LONDON_EXPORTS" | \
-   jq \
-   --arg EXPORT_NAME "account-resources-cdk-uk:Bucket:ArtifactsBucket:Arn" \
-   -r '.Exports[] | select(.Name == $EXPORT_NAME) | .Value')
-export artifact_bucket
 
-cloud_formation_execution_role=$(echo "$CF_LONDON_EXPORTS" | \
+ARTIFACT_BUCKET_ARN=$(echo "$CF_LONDON_EXPORTS" | \
+    jq \
+    --arg EXPORT_NAME "account-resources-cdk-uk:Bucket:ArtifactsBucket:Arn" \
+    -r '.Exports[] | select(.Name == $EXPORT_NAME) | .Value')
+ARTIFACT_BUCKET_NAME=$(echo "${ARTIFACT_BUCKET_ARN}" | cut -d ":" -f 6)
+
+if [ -z "${ARTIFACT_BUCKET_NAME}" ]; then
+    echo "could not retrieve ARTIFACT_BUCKET_NAME from aws cloudformation list-exports"
+    exit 1
+fi
+
+CLOUD_FORMATION_EXECUTION_ROLE=$(echo "$CF_LONDON_EXPORTS" | \
     jq \
     --arg EXPORT_NAME "iam-cdk:IAM:CloudFormationExecutionRole:Arn" \
     -r '.Exports[] | select(.Name == $EXPORT_NAME) | .Value')
 
-if [ -z "${cloud_formation_execution_role}" ]; then
-    echo "could not retrieve ROLE from aws cloudformation list-exports"
+if [ -z "${CLOUD_FORMATION_EXECUTION_ROLE}" ]; then
+    echo "could not retrieve CLOUD_FORMATION_EXECUTION_ROLE from aws cloudformation list-exports"
     exit 1
 fi
-export cloud_formation_execution_role
+
 TRUSTSTORE_BUCKET_ARN=$(echo "$CF_LONDON_EXPORTS" | \
     jq \
     --arg EXPORT_NAME "account-resources-cdk-uk:Bucket:TrustStoreBucket:Arn" \
     -r '.Exports[] | select(.Name == $EXPORT_NAME) | .Value')
 TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
+
+if [ -z "${TRUSTSTORE_BUCKET_NAME}" ]; then
+    echo "could not retrieve TRUSTSTORE_BUCKET_NAME from aws cloudformation list-exports"
+    exit 1
+fi
+
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
+
+export ARTIFACT_BUCKET_NAME
+export CLOUD_FORMATION_EXECUTION_ROLE
 export LATEST_TRUSTSTORE_VERSION
 
 cd ../../.aws-sam/build || exit
@@ -43,11 +58,11 @@ sam deploy \
     --stack-name "$STACK_NAME" \
     --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
     --region eu-west-2 \
-    --s3-bucket "$artifact_bucket" \
+    --s3-bucket "$ARTIFACT_BUCKET_NAME" \
     --s3-prefix "$ARTIFACT_BUCKET_PREFIX" \
     --config-file samconfig_package_and_deploy.toml \
     --no-fail-on-empty-changeset \
-    --role-arn "$cloud_formation_execution_role" \
+    --role-arn "$CLOUD_FORMATION_EXECUTION_ROLE" \
     --no-confirm-changeset \
     --force-upload \
     --tags "version=$VERSION_NUMBER stack=$STACK_NAME repo=$REPO cfnDriftDetectionGroup=$CFN_DRIFT_DETECTION_GROUP" \
